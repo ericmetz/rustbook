eed0b8a981087eb6208700541a2c14ce637fda8d
Eric <eric.metz07@gmail.com>
2026-02-07 13:11:30 +0100

    Rustbook Übung

---
+++ trait_objects/src/lib.rs
@@ -0,0 +1,44 @@
+pub trait Draw {
+    fn draw(&self);
+
+}
+
+// pub struct Screen<T: Draw> {
+//     pub components: Vec<T>,
+// }
+
+// impl<T> Screen<T>
+// where   
+//     T: Draw,
+// {
+//     pub fn run (&self) {
+//         for component in self.components.iter() {
+//             component.draw();
+//         }
+//     }
+// }
+
+pub struct Screen {
+    pub components: Vec<Box<dyn Draw>>,
+}
+
+impl Screen {
+    pub fn run(&self) {
+        for component in self.components.iter() {
+            component.draw();
+        }
+    }
+
+}
+
+pub struct Button {
+    pub width: u32,
+    pub height: u32,
+    pub lable: String,
+}
+
+impl Draw for Button {
+    fn draw(&self) {
+        // draw button
+    }
+}
---
+++ trait_objects/src/main.rs
@@ -0,0 +1,38 @@
+use trait_objects::{Screen, Button, Draw};
+
+struct SelectBox {
+    width: u32,
+    height: u32,
+    options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        // draw select box
+    }
+}
+
+
+fn main() {
+    let screen = Screen {
+        components: vec![
+            // Box::new(String::from("test")),
+            Box::new(SelectBox {
+                width: 100,
+                height: 100,
+                options: vec![
+                    String::from("yes"),
+                    String::from("no"),
+                    String::from("maybe"),
+                ]
+            }),
+            Box::new(Button {
+                width: 100,
+                height: 100,
+                lable: String::from("ok")
+            })
+        ]
+    };
+
+    screen.run();
+}
